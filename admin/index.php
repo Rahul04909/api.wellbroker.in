974199dad30497f79c75f8161f1187d6b5078936
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-07-10 00:38:07 +0530

    updated

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, OPTIONS');
